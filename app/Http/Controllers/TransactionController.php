a1e7c57a003c52f675385e07e67286495bfbba40
henyumino <henyumino37@gmail.com>
2021-06-18 17:34:39 +0800

    add transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -39,13 +39,59 @@ class TransactionController extends Controller
                 'country'   => $request->country,
                 'province'  => $request->province,
                 'city'      => $request->city,
-                'phone_number' => $request->phone_number
+                'phone_number' => $request->phone_number,
+                'resi' => '-',
+                'status' => 0
             ]);
-            
+
+            //status 0 => pending | 1 => accepted | 2 =>  rejected
             
             $response = ['success' => true];
             return response($response, 200);
         }
          
+    }
+
+    public function show(Request $request)
+    {
+        $uid = $request->user()->id;
+        $ts = Transaction::where('user_id',$uid)->get();
+        return response($ts, 200);
+    }
+
+    public function detail(Request $request, $id)
+    {
+        $uid = $request->user()->id;   
+        $ts = Transaction::findOrFail($id);
+        // with user, fix ini
+        
+        if($ts->user_id == $uid){
+            $res = [
+                'req' => $ts,
+                'item' => unserialize($ts->item),
+                'found' => true
+            ];
+            return response($res, 200);
+        }
+        else{
+            $res = [
+                'found' => false,
+                'message' => 'not found'
+            ];
+            return response($res, 200);
+        }
+
+    }
+
+    public function allTrans()
+    {
+        $ts = Transaction::all();
+        return response($ts, 200);
+    }
+
+    public function inputResi(Request $request, $id)
+    {
+        //tinggal tambahkan validasi
+        return response($request->resi,200);
     }
 }
