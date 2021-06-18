81af9ed178669fe830d5ec5453785bcf751b648d
henyumino <henyumino37@gmail.com>
2021-06-18 17:34:51 +0800

    add transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -21,5 +22,13 @@ class Transaction extends Model
         'province',
         'city',
         'phone_number',
+        'resi',
+        'status'
     ];
+
+    public function getCreatedAtAttribute(){
+        return Carbon::parse($this->attributes['created_at'])
+        ->format('Y/m/d');
+    }
+
 }
